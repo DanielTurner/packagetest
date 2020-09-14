b39dd656f0c5579de709eb25725510b487e24d2d
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-09-14 16:10:15 +1000

    I hope this is right, just building angular 1.0.3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { esModule } from './esmodule';
-const req = require('./commonjs.js');
-
-// esModule();
-req.commoner();
 
 let test = new esModule()
+
+require('angular');
+
+angular.module('testApp', [])
+angular.module('testApp').controller('testController', function() {
+  this.hello = 'hello 1'
+  console.log('hello 1')
+})
